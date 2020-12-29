e8aba55985b29e1ea404bf976c1d8258ca90c686
Tala Jallad <talaj2911@gmail.com>
2020-12-29 03:45:08 +0200

    5 questions added

---
+++ js/app.js
@@ -0,0 +1,51 @@
+'use strict';
+var username = prompt ("What is your name?")
+alert ("Welcome "+username)
+
+alert ("let's start the quiz!")
+
+var statement1= prompt ("I graduated university in 2017. (yes/no)")
+if (statement1.toLowerCase === "yes") {
+    //console.log ("You are correct!")
+    alert ("You are correct!")
+} else {
+    //console.log ("You are incorrect :(")
+    alert ("You are incorrect :(")
+}
+var statement2 = prompt ("I enjoy watching movies. (yes/no)")
+if (statement2.toLowerCase === "yes") {
+    //console.log ("You are correct!")
+    alert ("You are correct!")
+} else {
+    //console.log ("You are incorrect :(")
+    alert ("You are incorrect :(")
+}
+var statement3 = prompt ("I want to work as a software developer. (yes/no)")
+if (statement3.toLowerCase === "yes") {
+    //console.log ("You are correct!")
+    alert ("You are correct!")
+} else {
+    //console.log ("You are incorrect :(")
+    alert ("You are incorrect :(")
+}
+var statement4 = prompt ("I am 20 year old. (yes/no)")
+if (statement4.toLowerCase === "yes") {
+    //console.log ("You are correct!")
+    alert ("You are correct!")
+} else {
+    //console.log ("You are incorrect :(")
+    alert ("You are incorrect :(")
+}
+var statement5 = prompt (" I have been a team leader.(yes/no)")
+if (statement5.toLowerCase === "yes") {
+    //console.log ("You are correct!")
+    alert ("You are correct!")
+} else {
+    //console.log ("You are incorrect :(")
+    alert ("You are incorrect :(")
+}
+
+alert ("Thank you for doing the quiz "+username+"!")
+
+
+
